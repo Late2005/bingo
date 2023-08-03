6af5fdfa78f1ddaf7355643f9413730652ea8ede
Marc van de Laar <marcvdlaar@hotmail.com>
2023-08-03 14:29:20 +0200

    Bingo versie 1.1

--- bingo.py
+++ bingo.py
@@ -1,39 +1,44 @@
-from generator import *
-from exit import *
+from random import randint
+
 # My free Bingo Generator
-bingo = random()
-status = "a"
+status = 1
 
 def shell():
     global status
-    while status == "a":
-        print("Wilt u de bingo starten: y/n?")
-        answer = str(input("y/n?:"))
-        
-        if answer == "y":
+    while status == 1:
+        print("Wilt u de bingo starten: j/n?")
+        answer = str(input("j/n?:"))
+        if answer == "j":
             random()
             output()
         if answer == "n":
             exit()
-            status = "x"
+            status = 0
         else:
             shell()
 
+def random():
+    global bingo
+    bingo = []
+    for i in range(90):
+        n = randint(1,90)
+        if n not in bingo:        
+            bingo.append(n)
+    return bingo
+
 def output():
-    global status
     n = 0
     while n < (len(bingo)-1):
         print(bingo[n])
         doorgaan = str(input("Doorgaan? Druk op 'Enter', b, of 'q':"))
-        
+    
         if doorgaan == "b":
             print("Dit zijn de getrokken getallen:")
             print(bingo[:(n+1)])
         if doorgaan == "q":
             exit()
-            status = "x"
             break
-        if doorgaan == "":
+        else:
             n = n + 1
     else:
         print("Dit waren alle getallen van links naar rechts:")
@@ -42,6 +47,10 @@ def output():
         bingo[4::columns], bingo[5::columns], bingo[6::columns], bingo[7::columns], bingo[8::columns]):
             print(f'{a: <10}{b: <10}{c: <10}{d: <10}{e: <10}{f: <10}{g: <10}{h: <10}{i}')
         exit()
-        status = "x"
+
+def exit():
+    global status
+    status = 0
+    print("Bedankt voor het spelen van Bingo!")
 
 shell()
--- exit.py
+++
@@ -1,5 +0,0 @@
-def exit():
-    global status
-    status = "x"
-    print("Bedankt voor het spelen van Bingo!")
-    
